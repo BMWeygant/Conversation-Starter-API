853fa6c4e46012c4824c9ad3d37609cd32ee33e8
Brandon Weygant <bobbydigital108@gmail.com>
2019-08-05 14:26:26 -0400

    Added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :you, only: [:index, :create, :show]
+  resources :target, only: [:index, :create, :show]
+  resources :convo, only: [:index, :create, :show]
 end
