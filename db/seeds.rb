9652877f27d263faf06b706df85e749b1b862986
Brandon Weygant <bobbydigital108@gmail.com>
2019-08-06 15:11:59 -0400

    Added rack-cors gem, uncommented neccessarry code & added origins url

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,9 +10,9 @@ You.create(gender: 'male', swag: 'Swagjestic', conscience: 1, romantic_interest:
 You.create(gender: 'female', swag: 'Swagnaminous', conscience: 2, romantic_interest: true)
 You.create(gender: 'non-binary', swag: 'Swagverage', conscience: 3, romantic_interest: true)
 
-Target.create(gender: 'female' swag: 'SubSwagdard', relationship: 'Familia', appearance: 'Ratchet')
-Target.create(gender: 'male' swag: 'ASwagxual', relationship: 'none', appearance: 'Professional')
-Target.create(gender: 'non-binary' swag: 'Swagjestic', relationship: 'friend', appearance: 'Cute')
+Target.create(gender: 'female', swag: 'SubSwagdard', relationship: 'Familia', appearance: 'Ratchet')
+Target.create(gender: 'male', swag: 'ASwagxual', relationship: 'none', appearance: 'Professional')
+Target.create(gender: 'non-binary', swag: 'Swagjestic', relationship: 'friend', appearance: 'Cute')
 
 Convo.create(topic: '', location: 'Family Home', witnesses: true, ice_breaker: '')
 Convo.create(topic: '', location: 'Work', witnesses: true, ice_breaker: '')
